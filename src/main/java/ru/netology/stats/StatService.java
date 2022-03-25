329fb11d2d35c731b6e59dc731c5b54662d72639
aleksandraurchenko <alex_bloom@mail.ru>
2022-03-25 15:44:02 +0300

    Fixed

--- src/main/java/ru/netology/stats/StatService.java
+++ src/main/java/ru/netology/stats/StatService.java
@@ -39,9 +39,8 @@ public class StatService {
 
     public int lowerAvg(long[] sales) {
         int counter = 0;
-        long avg = sum(sales) / sales.length;
         for (long sale : sales) {
-            if (sale < avg) {
+            if (sale < average(sales)) {
                 counter = counter + 1;
             }
         }
